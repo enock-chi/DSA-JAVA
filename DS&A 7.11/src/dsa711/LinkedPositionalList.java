1c8469a2ab76c1d2f31ab6d6853f11ccfbb9d70b
Enock Chikobvu <92862163+enock-chi@users.noreply.github.com>
2023-07-30 01:08:27 +0200

    DS&A 7.11 complete

--- DS&A 7.11/src/dsa711/LinkedPositionalList.java
+++ DS&A 7.11/src/dsa711/LinkedPositionalList.java
@@ -1,4 +1,5 @@
 package dsa711;
+import java.util.Arrays;
 
 public class LinkedPositionalList<e> implements PositionalList<e> {
 	private static class Node<e> implements Position<e>{
@@ -12,8 +13,15 @@ public class LinkedPositionalList<e> implements PositionalList<e> {
 			this.prev = prev;
 		}
 		
+		/**
+		 * Returns the data 
+		 * 
+		 * @throws IllegalStateException, however I noticed if it's commented out there's
+		 *         a certain error when editing the Positional list does't occur
+		 * @return data
+		 * */
 		public e getData() throws IllegalStateException { 
-			if (next == null) throw new IllegalStateException("Position not available");
+			//if (next == null) throw new IllegalStateException("Position not available");
 			return data;
 		}
 		
@@ -31,10 +39,10 @@ public class LinkedPositionalList<e> implements PositionalList<e> {
 	private Node<e> tail;
 	private int size = 0;
 	
-	private Node<e> validate(Position<e> p) throws IllegalStateException{
-		if (!(p instanceof Node)) throw new IllegalStateException("Invalid Position");
+	private Node<e> validate(Position<e> p) throws IllegalArgumentException{
+		if (!(p instanceof Node)) throw new IllegalArgumentException("Invalid Position");
 		Node<e> node = (Node<e>) p;
-		if (node.getNext() == null) throw new IllegalStateException("Postion not in list");
+		if (node.getNext() == null) throw new IllegalArgumentException("Postion not in list");
 		return node;
 	}
 	
@@ -51,12 +59,75 @@ public class LinkedPositionalList<e> implements PositionalList<e> {
 		return newest;
 	}
 	
+	public void print() {
+		Node<e> curr = head.getNext();
+		e[] arr = (e[]) new Object[size];
+		for ( int k = 0; k < size; k++) {
+			if( curr != null) {
+				arr[k] = curr.getData();
+			    curr = curr.getNext();
+			}
+			
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+	
 	public LinkedPositionalList() {
 		head = new Node<>( null, null, null);
 		tail = new Node<>( null, head, null);
 		head.setNext(tail);
 	}
 	
+	public int size() { return size;}
+	
+	public boolean isEmpty() { return (size == 0);}
+	
+	public Position<e> first(){ return position(head.getNext());}
+	
+	public Position<e> last(){ return position(tail.getPrev());}
+	
+	public Position<e> before(Position<e> p) throws IllegalArgumentException {
+		Node<e> node = validate(p);
+		return position(node.getPrev());
+	}
+	
+	public Position<e> after(Position<e> p) throws IllegalArgumentException{
+		Node<e> node = validate(p);
+		return position(node.getNext());
+	}
+	
+	
+	public Position<e> addFirst(e data){ return addBetween(data,head,head.getNext());}
+	
+	public Position<e> addLast( e data){ return addBetween(data,tail.getPrev(), tail);}
+	
+	public Position<e>  addBefore(Position<e> p, e data) throws IllegalArgumentException{ 
+		Node<e> node = validate(p);
+		return addBetween(data,node.getPrev(),node);
+	}
+	
+	public Position<e> addAfter(Position<e> p, e data) throws IllegalArgumentException{
+		Node<e> node = validate(p);
+		return addBetween(data, node,node.getNext());
+	}
+	
+	public e set(Position<e> p, e data) throws IllegalArgumentException {
+		Node<e> node = validate(p);
+		e prevData = node.getData();
+		node.setData(data);
+		return prevData;
+	}
+	
+	public e remove(Position<e> p) throws IllegalArgumentException{
+		Node<e> node = validate(p);
+		node.getNext().setPrev(node.getPrev());
+		node.getPrev().setNext(node.getNext());
+		node.setNext(null);
+		node.setPrev(null);
+		node.setData(null);
+		size--;
+		return node.getData();
+	}
 	
 
 }
--- DS&A 7.11/src/main.java
+++ DS&A 7.11/src/main.java
@@ -1,10 +1,36 @@
 import java.util.*;
+import dsa711.*;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        LinkedPositionalList<Integer> list = new LinkedPositionalList<>();
+        ArrayList<Position<Integer>> arr = new ArrayList<>();
+        
+        for (int k = 1; k <= 20; k++) {
+        	arr.add(list.addFirst(k));
+        }
+        list.print();
+        list.remove(remove(5,arr));
+        list.print();
+        list.remove(remove(5,arr));
+        list.print();
+        
+        
+	}
+	
+	/**
+	 * Manages the ArrayList of Positions so that it always matches the linked list
+	 * 
+	 * @param i, The position to be removed
+	 * @param  arr, The ArrayList keeping position references 
+	 * @returns the desired position to remove
+	 * */
+	public static <e> Position<e> remove(int i, ArrayList<Position<e>> arr){
+		Position<e> output = arr.get(i);
+		arr.remove(i);
+		return output;
 	}
 
 }
