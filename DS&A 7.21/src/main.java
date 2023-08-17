1684771893852ff5230057923709526303f4311b
Enock Chikobvu <92862163+enock-chi@users.noreply.github.com>
2023-08-17 11:00:48 +0200

    Update main.java

--- DS&A 7.21/src/main.java
+++ DS&A 7.21/src/main.java
@@ -24,19 +24,30 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		MTF<Integer> mtf = new MTF<>();
+		MTF<String> mtf = new MTF<>();
 		//ArrayList<Position<MTF.Item<Integer>>> pList = new ArrayList<>();
 		
-		for (int k = 1; k <= 5; k++) {
-			mtf.add(k);
-		}
+		mtf.add("a");
+		mtf.add("b");
+		mtf.add("c");
+		mtf.add("d");
+		mtf.add("e");
+		mtf.add("f");
 		
-		mtf.print();
-		
-		mtf.access(3);
-		
-		mtf.print();
-
+        mtf.access("a");
+        mtf.access("b");
+        mtf.access("c");
+        mtf.access("d");
+        mtf.access("e");
+        mtf.access("f");
+        mtf.access("a");
+        mtf.access("c");
+        mtf.access("f");
+        mtf.access("b");
+        mtf.access("d");
+        mtf.access("e");
+        
+        mtf.print();
 	}
 
 }
