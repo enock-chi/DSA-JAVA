9475833f2dbb96bc276f2df009ef827bd38fa541
Enock Chikobvu <92862163+enock-chi@users.noreply.github.com>
2023-10-06 21:27:15 +0200

    DS&A 7.48 COMPLETE

--- DS&A 7.48/src/main.java
+++ DS&A 7.48/src/main.java
@@ -1,3 +1,25 @@
+/**
+ * @author Enock Chikobvu
+ * 
+ * { @code private Position<E> addBetween(E data, Node<E> prev, Node<E> next){
+				Node<E> newest = new Node<>(data,prev,next);
+				if ( size == 0) {
+					head = newest;
+					newest.setNext(head);
+					newest.setPrev(head);
+				} else {
+					prev.setNext(newest);
+				    next.setPrev(newest);
+				}
+				size++;
+				return newest;
+			}
+   }
+   
+   { @literal The code written above works fine for both add last and first however keep in mind
+              it's a circular so if printing with refernce to head it will print different.
+ */
+
 import dsa748.*;
 import java.util.*;
 
@@ -6,22 +28,14 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		LinkedPositionalList<Integer> list = new LinkedPositionalList<>();
-		ArrayList<Position<Integer>> plist  = new ArrayList<>();
-		Position<Integer> store;
 		
 		for (int k = 1; k <= 10; k++) {
-			plist.add(list.addFirst(k));
+			list.addFirst(k);
 		}
 		
 		list.print();
 		
-		store = list.before(plist.get(0));
 		
-		System.out.println(store.getData());
-		
-		System.out.println(list.first().getData());
-		
-		System.out.println(list.last().getData());
 	}
 
 }
